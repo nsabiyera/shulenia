2d3961f867864215cedb0f5cc055c30650ec3341
Ronald Nsabiyera <r.nsabiyera@gmail.com>
2016-11-03 18:10:11 +0000

    sample classes added

---
+++ src/main/java/com/shulenia/account/Account.java
@@ -0,0 +1,7 @@
+package com.shulenia.account;
+
+/**
+ * Created by Ronald on 11/3/2016.
+ */
+public class Account {
+}
---
+++ src/main/java/com/shulenia/core/AccountApi.kt
@@ -0,0 +1,7 @@
+package com.shulenia.core
+
+/**
+ * Created by Ronald on 11/3/2016.
+ */
+class CreateAccountCommand(val accountId: String, val overdraftLimit:Int)
+
