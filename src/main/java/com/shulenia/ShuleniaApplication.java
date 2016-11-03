f525246c83b0df0042eadfeff63a685664cc214a
Ronald Nsabiyera <r.nsabiyera@gmail.com>
2016-11-03 17:49:35 +0000

    initialise shulenia project

---
+++ src/main/java/com/shulenia/ShuleniaApplication.java
@@ -0,0 +1,12 @@
+package com.shulenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuleniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuleniaApplication.class, args);
+	}
+}
